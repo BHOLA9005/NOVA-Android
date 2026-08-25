07eebc15ab7788b898cbdc5f63e8ef3039406857
BHOLA9005 <b8152896@gmail.com>
2026-08-25 11:38:07 +0530

    Fix Android Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
 }
+
 android {
     namespace = "com.nova.assistant"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.nova.assistant"
         minSdk = 26
@@ -13,6 +15,7 @@ android {
         versionName = "1.0"
     }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
